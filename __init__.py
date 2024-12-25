6eb1de0aa35fa9c97577ba4b24a86e373853243c
小萌新 <3209970865@qq.com>
2024-12-25 11:17:30 +0800

    Feat: update version 0.3.6 -> 0.3.7

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "LightHelper",
     "author": "ACGGit Community,Atticus,小萌新",
     "blender": (4, 0, 0),
-    "version": (0, 3, 6),
+    "version": (0, 3, 7),
     "category": "Lighting",
     "support": "COMMUNITY",
     "doc_url": "",
